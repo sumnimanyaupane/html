d18e645ef25c640339be40829c28e9cdcc1c71cb
sumnimanyaupane <shovaneupane189@gmail.com>
2022-08-06 13:36:02 +0545

    First Commit

---
+++ Web/test2.cs
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>test2</title>
+    </head>
+    <body>
+        <div class=p1>
+         <p> nepal commerce campus nepal commerce campus nepal commerce campus nepal commerce campus nepal commerce campus nepal commerce campus vnepal commerce campus 
+            nepal commerce campus nepal commerce campus nepal commerce campus nepal commerce campus nepal commerce campus nepal commerce campus nepal commerce campus </p>
+            </div>
+            <div class="p2">
+            <p>nepal commerce campus nepal commerce campus nepal commerce campus nepal commerce campus nepal commerce campus nepal commerce campus 
+                nepal commerce campus nepal commerce campus nepal commerce campus nepal commerce campus nepal commerce campus nepal commerce campus     </p>
+                </div>
+                <style type="text/css">
+                    .p1{
+                        border:1px solid blue;
+                        width:2px;
+                        font-style: arial;
+                        font-colour :red;                       
+
+                    }
+                    
+                </style>
